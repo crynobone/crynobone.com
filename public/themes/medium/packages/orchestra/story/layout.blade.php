b18017d8018c070a2418b6072679ff2182738bd6
crynobone <crynobone@gmail.com>
2015-01-14 23:38:54 +0800

    Finalize on theme.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ public/themes/medium/packages/orchestra/story/layout.blade.php
@@ -0,0 +1,8 @@
+@include('orchestra/story::meta')
+@include('orchestra/story::header')
+
+<section id="river" role="main">
+	@yield('content')
+</section>
+
+@include('orchestra/story::footer')
---
+++ resources/themes/medium/packages/orchestra/story/meta.blade.php
@@ -0,0 +1,35 @@
+@section('sidebar')
+<iframe src="http://ghbtns.com/github-btn.html?user=crynobone&amp;type=follow&amp;count=true"
+    allowtransparency="true" frameborder="0" scrolling="0" width="165" height="20"></iframe>
+@stop
+
+@section('comments')
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'crynobone-com'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+<a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+@stop
+
+@push('asset.footer')
+<script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-9170846-19']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+</script>
+@endpush
